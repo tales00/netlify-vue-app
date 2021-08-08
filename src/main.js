3068f8fbae9123ebfd889dc6b8e3c9fcb0c2d4ce
tales <talesworks@gmail.com>
2021-08-09 05:58:28 +0800

    add - SCSS structure

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+//css
+import "sanitize.css";
+import "line-awesome/dist/line-awesome/css/line-awesome.min.css";
+import "@/scss/base.scss";
+
 createApp(App).mount("#app");
